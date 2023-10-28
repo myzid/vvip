0df0c095a33890c3ccdb0c6c55f0a71da4a53e6e
FadlyVPN <143302655+myzid@users.noreply.github.com>
2023-10-29 06:50:02 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 sysctl -w net.ipv6.conf.all.disable_ipv6=1 
 sysctl -w net.ipv6.conf.default.disable_ipv6=1 && apt update 
 date=$(date -R | cut -d " " -f -5)
-IP=$(wget -qO- ipinfo.io/ip);
-domain=$(cat /etc/xray/domain)
+scriptku="https://raw.githubusercontent.com/myzid/izin/main/ip"
+MYIP=$(wget -qO- ipinfo.io/ip);
 date=$(date +"%Y-%m-%d")
 clear
 red='\e[1;31m'
@@ -16,6 +16,8 @@ tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+
+
 cd /root
 #System version number
 if [ "${EUID}" -ne 0 ]; then
@@ -34,11 +36,68 @@ if [[ "$hst" != "$dart" ]]; then
 echo "$localip $(hostname)" >> /etc/hosts
 fi
 
-echo
-  read -p "[ ${green}NOTES${NC} ] Untuk Melanjutkan Penginstallan Klik Enter ! "
-  sleep 3
-  echo -e "[ ${green}INFO${NC} ] Oke, Melanjutkan Penginstallan"
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/myzid/izin/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBeli Akses Script Tunneling${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/fv_stores"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6283160098834"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit 0
+  fi
+}
+checking_sc
+clear
+# USERNAME
+rm -f /usr/bin/user
+username=$(curl ${scriptku} | grep $MYIP | awk '{print $2}')
+echo "$username" >/usr/bin/user
+# validity
+rm -f /usr/bin/e
+valid=$(curl ${scriptku} | grep $MYIP | awk '{print $3}')
+echo "$valid" >/usr/bin/e
+# DETAIL ORDER
+username=$(cat /usr/bin/user)
+oid=$(cat /usr/bin/ver)
+exp=$(cat /usr/bin/e)
+clear
+# CERTIFICATE STATUS
+d1=$(date -d "$valid" +%s)
+d2=$(date -d "$today" +%s)
+certifacate=$(((d1 - d2) / 86400))
+# VPS Information
+DATE=$(date +'%Y-%m-%d')
+datediff() {
+    d1=$(date -d "$1" +%s)
+    d2=$(date -d "$2" +%s)
+    echo -e "$COLOR1 $NC Expiry In   : $(( (d1 - d2) / 86400 )) Days"
+}
+mai="datediff "$Exp" "$DATE""
 
+# Status Expired Or Active 
+Info="(${green}Active${NC})"
+Error="(${RED}ExpiRED${NC})"
+today=`date -d "0 days" +"%Y-%m-%d"`
+Exp1=$(curl ${scriptku} | grep $MYIP | awk '{print $3}')
+if [[ $today < $Exp1 ]]; then
+sts="${Info}"
+else
+sts="${Error}"
+fi
+clear
 
 secs_to_human() {
     echo "Installation time : $(( ${1} / 3600 )) hours $(( (${1} / 60) % 60 )) minute's $(( ${1} % 60 )) seconds"
@@ -89,7 +148,7 @@ touch /etc/xray/scdomain
 touch /etc/v2ray/scdomain
 
 wget -q https://raw.githubusercontent.com/myzid/vvip/main/tools.sh && chmod +x tools.sh && ./tools.sh
-rm tools.sh
+rm -rf tools.sh
 apt install wget -y
 apt install curl -y
 apt install git curl -y >/dev/null 2>&1
@@ -113,6 +172,8 @@ gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | 
     chronyd -q 'server 0.id.pool.ntp.org iburst'
     chronyc sourcestats -v
     chronyc tracking -v
+    wget https://raw.githubusercontent.com/FadlyNotNot/v1/main/bbr.sh ; chmod +x bbr.sh ; ./bbr.sh
+    rm -rf bbr.sh
 
 clear
 echo -e "$green━━━━━━━━━━┏┓━━━━━━━━━━━━━━━━━━━━━━━━┏┓━━━━━━━━━━━$NC"
@@ -136,6 +197,9 @@ read -rp " Choose Potions 1 or 2 / pilih 1 atau 2 : " dns
     chmod +x /root/cf
     bash /root/cf | tee /root/install.log
 	elif test $dns -eq 2; then
+    clear
+    echo -e " \e[1;33mSEBELUM MEMASUKAN DOMAIN, HARAP POINTING DULU IP VPS KAMU !\e[0m"
+    echo ""
     read -rp "Masukan Domain Kamu : " pp
     echo "$pp" > /root/scdomain
 	echo "$pp" > /etc/xray/scdomain
@@ -145,8 +209,35 @@ read -rp " Choose Potions 1 or 2 / pilih 1 atau 2 : " dns
     echo "IP=$pp" > /var/lib/SIJA/ipvps.conf
     fi
 
+    tgl2=$(date +"%d")
+    bln2=$(date +"%b")
+    thn2=$(date +"%Y")
+    tnggl="$tgl2 $bln2, $thn2"
+    MYIP=$(curl -sS ipv4.icanhazip.com)
+    domain=$(cat /etc/xray/domain)
+    CHATID="-1001899398362"
+    KEY="6293396608:AAGqZVrmdQjPc3tOj_gnUoWOVMrBsm8v6Xo"
+    TIMES="10"
+    URL="https://api.telegram.org/bot$KEY/sendMessage"
+    TIMEZONE=$(printf '%(%H:%M:%S)T')
+    TEXT="
+<code>────────────────────</code>
+<b>⚡AUTOSCRIPT PREMIUM⚡</b>
+<code>────────────────────</code>
+<code>ID     : </code><code>$username</code>
+<code>Domain : </code><code>$domain</code>
+<code>Date   : </code><code>$tnggl</code>
+<code>Time   : </code><code>$TIMEZONE</code>
+<code>Ip vps : </code><code>$MYIP</code>
+<code>Exp Sc : </code><code>$exp</code>
+<code>────────────────────</code>
+<i>Automatic Notification from Github</i>
+<i>Fvstores</i>
+"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ🧊","url":"https://t.me/fv_stores"},{"text":"ᴏʀᴅᴇʀ🧊","url":"wa.me/083160098834"}]]}'
+    curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
+
 clear 
-#install ssh-vpn
+#install ssh & ws python
 echo -e "┌─────────────────────────────────────────┐"
 echo -e " \E[41;1;97m         >>> Install SSH WS <<<         \E[0m$NC"
 echo -e "└─────────────────────────────────────────┘"
@@ -222,6 +313,10 @@ chmod +x /usr/bin/*
 cd 
 chmod +x /usr/bin/acs-set
 
+clear
+echo
+echo -e "${GREEN}Memasang Default Menu${NC}"
+sleep 2
 cat > /etc/cron.d/re_otm <<-END
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
@@ -244,9 +339,6 @@ cat > /home/re_otm <<-END
 7
 END
 
-service cron restart >/dev/null 2>&1
-service cron reload >/dev/null 2>&1
-
 clear
 cat> /root/.profile << END
 # ~/.profile: executed by Bourne-compatible login shells.
@@ -262,6 +354,13 @@ clear
 neofetch
 END
 chmod 644 /root/.profile
+echo
+echo -e "${GREEN}Default Menu Berhasil Di Pasang${NC}"
+sleep 2
+clear
+
+service cron restart >/dev/null 2>&1
+service cron reload >/dev/null 2>&1
 
 if [ -f "/root/log-install.txt" ]; then
 rm /root/log-install.txt > /dev/null 2>&1
@@ -286,11 +385,11 @@ echo " "
 echo "------------------------------------------------------------"
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH		: 22"  | tee -a log-install.txt
-echo "   - SSH Websocket	: 80" | tee -a log-install.txt
+echo "   - SSH Websocket	: 80, 8080, 8880, 2082" | tee -a log-install.txt
 echo "   - SSH SSL Websocket	: 443" | tee -a log-install.txt
 echo "   - Stunnel4		: 447, 777" | tee -a log-install.txt
 echo "   - Dropbear		: 109, 143" | tee -a log-install.txt
-echo "   - Badvpn		: 7100-7900" | tee -a log-install.txt
+echo "   - Badvpn		: 7100-7300" | tee -a log-install.txt
 echo "   - Nginx		: 81" | tee -a log-install.txt
 echo "   - Vmess    		: 443" | tee -a log-install.txt
 echo "   - Vmess None TLS       : 80" | tee -a log-install.log
