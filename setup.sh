bb846e1e15b724a04421dd265939ee787ed2e53e
FadlyVPN <143302655+myzid@users.noreply.github.com>
2023-10-29 08:20:59 +0700

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -157,11 +157,27 @@ touch /etc/v2ray/scdomain
 
 wget -q https://raw.githubusercontent.com/myzid/vvip/main/tools.sh && chmod +x tools.sh && ./tools.sh
 rm -rf tools.sh
+apt update -y
+apt upgrade -y
+apt dist-upgrade -y
+apt install sudo -y
+sudo apt-get clean all
+sudo apt-get install -y debconf-utils
+apt-get remove --purge ufw firewalld -y
+apt-get remove --purge exim4 -y
+apt-get autoremove -y
+sudo apt-get install -y --no-install-recommends software-properties-common
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+sudo apt-get install -y iptables iptables-persistent netfilter-persistent figlet ruby php php-fpm php-cli php-mysql libxml-parser-perl squid nmap screen curl jq bzip2 gzip coreutils rsyslog iftop htop zip unzip net-tools sed gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch screenfetch lsof openssl openvpn easy-rsa fail2ban tmux stunnel4 squid3 dropbear socat cron bash-completion ntpdate xz-utils apt-transport-https gnupg2 dnsutils lsb-release chrony libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev xl2tpd pptpd apt git speedtest-cli p7zip-full
+sudo apt-get install -y libjpeg-dev zlib1g-dev python python3 python3-pip shc build-essential speedtest-cli p7zip-full nodejs nginx
+apt install nginx -y
 apt install wget -y
 apt install curl -y
 apt install git curl -y >/dev/null 2>&1
 apt install python -y >/dev/null 2>&1
 apt install -y bzip2 gzip coreutils screen curl unzip
+
 
 gotop_latest="$(curl -s https://api.github.com/repos/xxxserxxx/gotop/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
     gotop_link="https://github.com/xxxserxxx/gotop/releases/download/v$gotop_latest/gotop_v"$gotop_latest"_linux_amd64.deb"
